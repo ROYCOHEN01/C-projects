255e34d5dca748f660ca94f72dcf6dcdb189def2
ROYCOHEN01 <134857058+ROYCOHEN01@users.noreply.github.com>
2024-07-02 12:29:35 +0300

    Add files via upload

---
+++ ex0_2_313293136.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("   /\\\n");
+	printf("  /  \\\n");
+	printf(" /    \\\n");
+	printf("/      \\\n");
+	printf("--------\n");
+	printf("|    _ |\n");
+	printf("|   |_||\n");
+	printf("| __   |\n");
+	printf("||  |  |\n");
+	printf("||  |__|\n");
+	return 0;
+}
